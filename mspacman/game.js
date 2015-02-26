00a7defe19474b208c4131d436b43bd2bc3045e3
Eric Peterson <ewpete@gmail.com>
2015-02-25 19:22:56 -0500

    added both layers of campus, resolved javascript issues

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,17 @@
-// Replace this with your code...
+function init() {
+    canvas = document.getElementById('game_canvas');
+
+    if (canvas.getContext) {
+        context = canvas.getContext('2d');
+        img = new Image();
+        img.src = 'pacman10-hp-sprite.png'
+
+        img.onload = function(){
+            context.drawImage(img, 322, 0, 464, 140, 0, 0, 464, 140);
+            context.drawImage(img, 100, 0, 20, 20, 2, 5, 20, 20);
+        }; 
+    }
+    else {
+        alert('Sorry, canvas is not supported on your browser!');
+    }  
+}     
