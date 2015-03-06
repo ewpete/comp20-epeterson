427c9026c39b3c3ff40ca11844171a343a0d5b1c
Eric Peterson <ewpete@gmail.com>
2015-03-06 15:29:18 -0500

    trouble with gh-pages, trying to change request type

--- messages/lab.js
+++ messages/lab.js
@@ -10,7 +10,7 @@ function parse() {
     //Send the proper header information along with the request
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     // request.setRequestHeader("Content-length", params.length);
-    request.setRequestHeader("Connection", "close");
+    // request.setRequestHeader("Connection", "close");
     
     request.send();  
 
