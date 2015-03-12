f5b63198c36f11bda17a0616fa438480099514fc
Eric Peterson <ewpete@gmail.com>
2015-03-11 23:07:10 -0400

    cleaned up and removed unnecessary code - added comments

--- mmap/map.js
+++ mmap/map.js
@@ -1,11 +1,19 @@
-myLat = 0;
-myLng = 0;
+//eric peterson
+//comp20
+//assignment 2 - marauder's map
+
+// map.js - javascript script to get current location, and request location of
+//          other users and display them on the map, along with 
+//          the distances from each user to me
+
+//initial myLat and myLng are Tufts'
+myLat = 42.4069;
+myLng =71.1198;
+
 var lmark2;
 var map;
 var marker;
 var mapOptions;
-
-var infowindow = new google.maps.InfoWindow();
 
 
 function initialize() {
@@ -20,50 +28,42 @@ function initialize() {
     getMyLocation();
 
 }
-
+// this function gets my location, sets my image and marker on the map,
+// and then calls HTTP_post to postmy location and receive info about 
+// everyone else in the class
 function getMyLocation() {
-    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+    if (navigator.geolocation) { // if you support geolocation...
         navigator.geolocation.getCurrentPosition(function(position) {
             myLat = position.coords.latitude;
             myLng = position.coords.longitude;
            
             // Pan to new location
-            map.panTo( new google.maps.LatLng(myLat, myLng ) );
+            map.panTo(new google.maps.LatLng(myLat, myLng));
 
             // Set the marker
-            lmark2 = new google.maps.LatLng(myLat,myLng);
-            var image = 'ness.png';
-            
-            var icon = new google.maps.MarkerImage(
-                image
-                )
+            my_position = new google.maps.LatLng(myLat,myLng); 
+            //set my image           
+            var my_icon = new google.maps.MarkerImage("ness.png");
 
             myName = "RickSoulen"
             marker = new google.maps.Marker({
-                position: lmark2,
+                position: my_position,
                 map: map,
                 title: myName,
-                icon: image
+                icon: my_icon
             });
+            //set my marker label when clicked
+            marker.content = '<div id="content"> <h3>' + myName + '</h3> </div>';
 
-            contentString = '<div id="content">'+
-              '<div id="siteNotice">'+
-              '</div>'+
-              '<h3 id="firstHeading" class="firstHeading">' + myName + '</h3>'+
-
-              '</div>'+
-              '</div>';
-
-
-            marker.content = contentString;
-            infowindow = new google.maps.InfoWindow();
-
+            my_info_window = new google.maps.InfoWindow();
+            // set the infowindow to pop up when clicking my marker
             google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(this.content);
-                infowindow.open(this.getMap(), this);
+                my_info_window.setContent(this.content);
+                my_info_window.open(this.getMap(), this);
             });
-            marker.setMap(map);
-
+            // now that all of my coordinates//marker//image//content are 
+            // complete, begin the second phase of the HTTP requesting
+            // to send my info and 
             HTTP_post(myLat, myLng, "RickSoulen");
         });
 
@@ -73,8 +73,11 @@ function getMyLocation() {
     }
 
 }
-
+// this function posts my position and my login to the server, and recieves
+// the response with the json object of everyone else in the class' location
+// and name
 function HTTP_post (myLat, myLong, myLogin) {
+    //initialize request
     var request = new XMLHttpRequest();
     var params = "login=" + myLogin + "&lat=" + myLat + "&lng=" + myLng;
     var url = "https://secret-about-box.herokuapp.com/sendLocation";
@@ -83,26 +86,33 @@ function HTTP_post (myLat, myLong, myLogin) {
     //Send the proper header information along with the request
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.send(params);  
-
+    //this function is called everytime a readystate change, and only calls
+    // the next function, process_response, when the final ready state is
+    // reached with an OK message (ie, 200)
     request.onreadystatechange = function() {//Call a function when the state changes.
         if(request.readyState == 4 && request.status == 200) {
+            //parse data
             data = JSON.parse(request.responseText);
-            process_response3(data);
+            process_response(data);
         }
     }
 
 }
-function process_response3(data) {
+
+//this function runs through every person in the JSON response and 
+// calls the helper function make_marker, to add them to the map
+function process_response(data) {
     length = data.length;
-    for (i = 0; i < length; i++) {  
+    for (i = 0; i < length; i++) {
         make_marker(data[i], "RickSoulen");
        
     }
 }
-
+// this function takes in a single JSON object and places their marker on 
+// the map
 function make_marker(data, myName) {
      login = data["login"];
-        if (login != myName) {
+        if (login != myName) { //ignore myself, i've already been added
             lat = data["lat"];
             lng = data["lng"];
             lat_lng = new google.maps.LatLng(lat, lng);
@@ -111,15 +121,17 @@ function make_marker(data, myName) {
                 map: map,
                 title: login
             });
-            d = Math.round(distance(myLat, myLng, lat, lng)*100) / 100;
+            //calculate the distance from me using the haversine formula
+            d = Math.round(haversine(myLat, myLng, lat, lng)*100) / 100;
             contentString = '<div id="content">'+
                 '<h3>' + login + '</h3>'+
                 '<p> Distance from ' + myName + ": " + d + " miles </p>"
-                '</div>' + '</div>';  
+                '</div>';  
             marker.content = contentString;
             infowindow = new google.maps.InfoWindow();
-
+            //make the marker 
             google.maps.event.addListener(marker, 'click', function() {
+                //this.XXX means marker.XXX in thie context
                 infowindow.setContent(this.content);
                 infowindow.open(this.getMap(), this);
             });
@@ -127,8 +139,9 @@ function make_marker(data, myName) {
 
 }
 
-
-function distance(myLat, myLng, theirLat, theirLng) {
+//this function takes in two coordinates and calculates their distance 
+//in miles - code taken from the web.
+function haversine(myLat, myLng, theirLat, theirLng) {
     Number.prototype.toRad = function() {
        return this * Math.PI / 180;
     }
@@ -153,47 +166,4 @@ function distance(myLat, myLng, theirLat, theirLng) {
     // alert(d);
     return d;
 }
-
-
-// function process_response(data) {
-//     length = data.length;
-//     windows = {}
-//     var s_marker, i;
-
-//     for (i = 0; i < length-4; i++) {  
-//         login = data[i]["login"];
-//         if (login != "RickSoulen") {
-//             windows[login] = {}
-//             lat = data[i]["lat"];
-//             lng = data[i]["lng"];
-//             lat_lng = new google.maps.LatLng(lat, lng);
-
-//             windows[login]["marker"] = new google.maps.Marker({
-//                 position: lat_lng,
-//                 map: map,
-//                 title: login
-//             });
-
-//             d = Math.round(distance(myLat, myLng, lat, lng)*100) / 100;
-//             windows[login]["contentString"] = '<div id="content">'+
-//                 '<h3>' + login + '</h3>'+
-//                 '<p> Distance from ' + myName + ": " + d + " miles </p>"
-//                 '</div>' + '</div>';  
-
-//             windows[login]["window"]= new google.maps.InfoWindow({
-//                 content: windows[login]["contentString"]
-//             });
-            
-//             google.maps.event.addListener(windows[login]["marker"], 'click', function() {
-//                 windows[login]["window"].open(map, windows[login]["marker"]);
-//             });
-
-//             // google.maps.event.addListener(s_marker, 'click', function() {
-//             //         infowindow.setContent(contentString);
-//             //         infowindow.open(map, s_marker);
-
-//             //     });
-//         }
-//     }
-// }
 
